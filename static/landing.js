977d416a96a0731448521690fd55a8f5e12c9991
unknown <Markus>
2024-10-10 18:59:30 +0300

    Rough sketch for landing page, missing the WebRTC functionalities

---
+++ static/landing.js
@@ -0,0 +1,20 @@
+var createRoomButton= document.querySelector('#create-room-btn'); 
+var joinRoomButton = document.querySelector('#join-room-btn'); 
+
+createRoomButton.addEventListener("click", function() {
+    const name = document.querySelector('#name').value; 
+    if (name) {
+        alert("Creating room for " + name);
+    } else {
+        alert("Please enter a name");
+    }
+});
+
+joinRoomButton.addEventListener("click", function() {
+    const name = document.querySelector('#name').value; 
+    if (!name) {
+        alert("Please enter a name");
+    } else {
+        const roomCode = prompt("Please enter the 4-letter room code:");
+    }
+});
